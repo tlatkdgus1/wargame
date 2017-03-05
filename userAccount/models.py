a495c19494d2bd1ffabb49fd1d897c7c914aeb81
Sim SangHyun <ipod1597@naver.com>
2017-03-05 16:09:22 +0900

    model

--- userAccount/models.py
+++ userAccount/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
-	pass
+	question = models.ManytoMany(Question)
 
 class Question(models.Model):
 	title = models.TextField(maxlength=20)
-	score = models.IntegerField()
+	score = models.IntegerField(default=0)
 	text = models.TextField(maxlength=300)
 	file = models.fileField(maxlength=20)
 	answer = models.TextField(maxlength=50)
