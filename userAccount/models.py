7c713bb5e47de8afc79a7d15e6402cac61773692
Sim SangHyun <ipod1597@naver.com>
2017-03-05 16:08:18 +0900

    model

--- userAccount/models.py
+++ userAccount/models.py
@@ -3,3 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
 	pass
+
+class Question(models.Model):
+	title = models.TextField(maxlength=20)
+	score = models.IntegerField()
+	text = models.TextField(maxlength=300)
+	file = models.fileField(maxlength=20)
+	answer = models.TextField(maxlength=50)
